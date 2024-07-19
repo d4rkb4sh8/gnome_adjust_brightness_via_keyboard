d0700696a7de58958d26d27fe72e67b22501dd0b
d4rkb4sh8 <145238010+d4rkb4sh8@users.noreply.github.com>
2024-07-18 20:29:21 -0500

    Update adjust_brightness.sh

--- adjust_brightness.sh
+++ adjust_brightness.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # add this to your bashrc or zshrc
 #I have chosen the following shortcuts as they don't conflict with any other 
 #keyboard shortcuts in debian but you can modify them to whatever you need. First try before you change.
